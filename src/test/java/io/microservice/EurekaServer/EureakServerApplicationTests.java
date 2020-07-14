00139dababbb9dab1d04c14e052439143ee33d71
Priyobrato Das <priyodas12@gmail.com>
2020-07-14 22:59:51 +0530

    Initial commit

---
+++ src/test/java/io/microservice/EurekaServer/EureakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microservice.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
